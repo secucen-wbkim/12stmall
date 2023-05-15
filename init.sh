e64fa6f89247c5513878d2bdb4ceb11a4f7f895a
secucen-wbkim <wbkim@secucen.com>
2023-05-15 05:45:05 +0000

    add kubectl alias

--- init.sh
+++ init.sh
@@ -6,6 +6,12 @@ pip install httpie
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+
+source <(kubectl completion bash) # bash-completion 패키지를 먼저 설치한 후, bash의 자동 완성을 현재 셸에 설정한다
+echo "source <(kubectl completion bash)" >> ~/.bashrc # 자동 완성을 bash 셸에 영구적으로 추가한다
+echo "alias k=kubectl" >> ~/.bashrc
+echo "complete -o default -F __start_kubectl k" >> ~/.bashrc
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "../awscliv2.zip"
 unzip -o ../awscliv2.zip -d ../
 sudo .././aws/install
@@ -16,3 +22,4 @@ sudo mv /tmp/eksctl /usr/local/bin
 cd kafka
 docker container rm kafka-kafka-1 kafka-zookeeper-1
 docker-compose up
+
